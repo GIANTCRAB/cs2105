91ccc7a42bb360bfc3457e17cf1c3a0f22c3a78b
Huiren Woo <giantcrabby@gmail.com>
2020-09-19 16:43:36 +0800

    Change to be single threaded

--- src/assignment1/WebServer.java
+++ src/assignment1/WebServer.java
@@ -12,6 +12,9 @@ import java.util.concurrent.ConcurrentMap;
 
 public class WebServer {
     private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private OutputStream out;
+    private InputStream in;
     private int portNumber;
 
     private static ConcurrentMap<String, byte[]> kvStore = new ConcurrentHashMap<>();
@@ -29,7 +32,13 @@ public class WebServer {
     public void start() throws IOException {
         serverSocket = new ServerSocket(this.portNumber);
         while (true) {
-            new EchoClientHandler(serverSocket.accept()).start();
+            clientSocket = serverSocket.accept();
+            out = clientSocket.getOutputStream();
+            in = clientSocket.getInputStream();
+            readPacket();
+            in.close();
+            out.close();
+            clientSocket.close();
         }
     }
 
@@ -37,86 +46,81 @@ public class WebServer {
         serverSocket.close();
     }
 
-    private static class EchoClientHandler extends Thread {
-        private final Socket clientSocket;
-        private OutputStream out;
-        private InputStream in;
+    public void readPacket() throws IOException {
+        int currByte;
+        char currChar;
+        // 2 continuous newlines = end of header
+        char headerBoundaryChar = '\n';
+        int continuousNewLineCount = 0;
+        final int maxContinuousNewLineCount = 2;
+        boolean headerComplete = false;
+        String header = "";
+        List<String> headerInfo = new ArrayList<>();
+        int payloadSizeBytes = 0;
+        int payloadIndex = 0;
+        byte[] payload = new byte[payloadSizeBytes];
+        // read data only if it is a POST
+        String storeType;
+        String keyToWrite = "";
 
-        public EchoClientHandler(Socket socket) throws IOException {
-            this.clientSocket = socket;
-            out = clientSocket.getOutputStream();
-            in = clientSocket.getInputStream();
-        }
-
-        public void run() {
-            try {
-                this.readPacket();
-                in.close();
-                out.close();
-                clientSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void readPacket() throws IOException {
-            int currByte;
-            char currChar;
-            // 2 continuous newlines = end of header
-            char headerBoundaryChar = '\n';
-            int continuousNewLineCount = 0;
-            final int maxContinuousNewLineCount = 2;
-            boolean headerComplete = false;
-            String header = "";
-            List<String> headerInfo = new ArrayList<>();
-            int payloadSizeBytes = 0;
-            int payloadIndex = 0;
-            byte[] payload = new byte[payloadSizeBytes];
-            // read data only if it is a POST
-            String storeType;
-            String keyToWrite = "";
-
-            // Read one byte at a time from stdin
-            while ((currByte = in.read()) != -1) { // not reached end of stream
-                if (0 == payloadSizeBytes) {
-                    // Get header
-                    currChar = (char) currByte;
-                    if (!headerComplete) {
-                        if (currChar != headerBoundaryChar) {
-                            header += String.valueOf(currChar);
-                            continuousNewLineCount = 0; // Reset continuous new line data
+        // Read one byte at a time from stdin
+        while ((currByte = in.read()) != -1) { // not reached end of stream
+            if (0 == payloadSizeBytes) {
+                // Get header
+                currChar = (char) currByte;
+                if (!headerComplete) {
+                    if (currChar != headerBoundaryChar) {
+                        header += String.valueOf(currChar);
+                        continuousNewLineCount = 0; // Reset continuous new line data
+                        continue;
+                    } else {
+                        continuousNewLineCount++;
+                        if (continuousNewLineCount < maxContinuousNewLineCount) {
+                            headerInfo.add(header);
+                            header = "";
                             continue;
                         } else {
-                            continuousNewLineCount++;
-                            if (continuousNewLineCount < maxContinuousNewLineCount) {
-                                headerInfo.add(header);
-                                header = "";
-                                continue;
-                            } else {
-                                headerComplete = true;
-                                // carry on to the next step of parsing the data, do not call continue
-                            }
+                            headerComplete = true;
+                            // carry on to the next step of parsing the data, do not call continue
                         }
                     }
+                }
 
-                    // Parsing header data
-                    final String responseType = headerInfo.get(0).toLowerCase();
-                    // Read store type
-                    final String[] pathData = headerInfo.get(1).split("/");
-                    // 0 is empty
-                    storeType = pathData[1];
-                    keyToWrite = pathData[2];
+                // Parsing header data
+                final String responseType = headerInfo.get(0).toLowerCase();
+                // Read store type
+                final String[] pathData = headerInfo.get(1).split("/");
+                // 0 is empty
+                storeType = pathData[1];
+                keyToWrite = pathData[2];
 
-                    if (headerInfo.size() < 4) {
-                        // no content-length in header
-                        if (storeType.toLowerCase().equals("counter")) {
-                            final int count = counterStore.getOrDefault(keyToWrite, 0);
-                            if (responseType.equals("get")) {
-                                printOkResponseWithContent(String.valueOf(count).getBytes());
+                if (headerInfo.size() < 4) {
+                    // no content-length in header
+                    if (storeType.toLowerCase().equals("counter")) {
+                        final int count = counterStore.getOrDefault(keyToWrite, 0);
+                        if (responseType.equals("get")) {
+                            printOkResponseWithContent(String.valueOf(count).getBytes());
+                        } else {
+                            // POST to increment
+                            counterStore.put(keyToWrite, count + 1);
+                            printOkResponseWithNoContent();
+                        }
+
+                        // Reset
+                        payloadSizeBytes = 0;
+                        header = "";
+                        headerInfo = new ArrayList<>();
+                        headerComplete = false;
+                        continuousNewLineCount = 0;
+                    } else {
+                        // key-value store
+                        if (responseType.equals("get")) {
+                            // retrieve data
+                            byte[] data = kvStore.get(keyToWrite);
+                            if (data == null) {
+                                printNotFoundResponse();
                             } else {
-                                // POST to increment
-                                counterStore.put(keyToWrite, count + 1);
-                                printOkResponseWithNoContent();
+                                printOkResponseWithContent(data);
                             }
 
                             // Reset
@@ -125,62 +129,44 @@ public class WebServer {
                             headerInfo = new ArrayList<>();
                             headerComplete = false;
                             continuousNewLineCount = 0;
-                        } else {
-                            // key-value store
-                            if (responseType.equals("get")) {
-                                // retrieve data
-                                byte[] data = kvStore.get(keyToWrite);
-                                if (data == null) {
-                                    printNotFoundResponse();
-                                } else {
-                                    printOkResponseWithContent(data);
-                                }
-
-                                // Reset
-                                payloadSizeBytes = 0;
-                                header = "";
-                                headerInfo = new ArrayList<>();
-                                headerComplete = false;
-                                continuousNewLineCount = 0;
-                            }
                         }
-                    } else {
-                        // Get payload size in bytes from header
-                        payloadSizeBytes = Integer.parseInt(headerInfo.get(3));
-                        payload = new byte[payloadSizeBytes];
-                        payloadIndex = 0;
                     }
-                    continue; // currChar will be headerBoundaryChar, hence go to next iteration to read 1st payload byte
+                } else {
+                    // Get payload size in bytes from header
+                    payloadSizeBytes = Integer.parseInt(headerInfo.get(3));
+                    payload = new byte[payloadSizeBytes];
+                    payloadIndex = 0;
                 }
+                continue; // currChar will be headerBoundaryChar, hence go to next iteration to read 1st payload byte
+            }
 
-                // Read 1 byte of payload
-                payload[payloadIndex] = (byte) currByte;
-                payloadIndex++;
-                if (payloadIndex >= payloadSizeBytes) { // finished reading payload
-                    kvStore.put(keyToWrite, payload);
-                    // Write out that it is done
-                    printOkResponseWithNoContent();
-                    // Reset
-                    payloadSizeBytes = 0;
-                    header = "";
-                    headerInfo = new ArrayList<>();
-                    headerComplete = false;
-                    continuousNewLineCount = 0;
-                }
+            // Read 1 byte of payload
+            payload[payloadIndex] = (byte) currByte;
+            payloadIndex++;
+            if (payloadIndex >= payloadSizeBytes) { // finished reading payload
+                kvStore.put(keyToWrite, payload);
+                // Write out that it is done
+                printOkResponseWithNoContent();
+                // Reset
+                payloadSizeBytes = 0;
+                header = "";
+                headerInfo = new ArrayList<>();
+                headerComplete = false;
+                continuousNewLineCount = 0;
             }
         }
+    }
 
-        public void printNotFoundResponse() throws IOException {
-            out.write("404\nNotFound\n\n".getBytes());
-        }
+    public void printNotFoundResponse() throws IOException {
+        out.write("404\nNotFound\n\n".getBytes());
+    }
 
-        public void printOkResponseWithNoContent() throws IOException {
-            out.write("200\nOK\n\n".getBytes());
-        }
+    public void printOkResponseWithNoContent() throws IOException {
+        out.write("200\nOK\n\n".getBytes());
+    }
 
-        public void printOkResponseWithContent(byte[] content) throws IOException {
-            out.write(("200\nOK\ncontent-length\n" + content.length + "\n\n").getBytes());
-            out.write(content);
-        }
+    public void printOkResponseWithContent(byte[] content) throws IOException {
+        out.write(("200\nOK\ncontent-length\n" + content.length + "\n\n").getBytes());
+        out.write(content);
     }
 }
